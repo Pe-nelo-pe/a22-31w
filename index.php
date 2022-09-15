b5d6477cc6fdf9af3d5e8c28566b916a4deb61f4
Pénélope Ducharme <88218376+Pe-nelo-pe@users.noreply.github.com>
2022-09-15 11:15:12 -0400

    Ajout de styles dans index.php

--- index.php
+++ index.php
@@ -22,7 +22,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Document</title>
+
+    <style>
+        body{
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        }
+
+        h1{
+            background-color: aqua;
+            color: #555;
+            font-size: 2rem;
+        }
+    </style>
 </head>
 <body>
     
