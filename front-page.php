881a5316837cf5d11eea3ef06db77a5ef5d422d2
Pénélope Ducharme <88218376+Pe-nelo-pe@users.noreply.github.com>
2022-09-29 12:33:01 -0400

    modifications visuelles du nav

--- front-page.php
+++ front-page.php
@@ -25,7 +25,7 @@
             if ( have_posts() ) :
                 while ( have_posts() ) :
                     the_post();?>
-                    <h1><a href="<?php the_permalink()?>"><?php the_title() ?></a></h1>
+                    <h2><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2 >
                     <?php the_content(null, true);
                   
                 endwhile;
