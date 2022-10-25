51f2f0547f4ec284f2f771aaeab3c2b197cb3376
Pénélope Ducharme <88218376+Pe-nelo-pe@users.noreply.github.com>
2022-10-25 15:49:07 -0400

    c13 - modifications du widget dans le footer

--- footer.php
+++ footer.php
@@ -12,7 +12,12 @@
 ?>
 
 	<footer class="site__footer">
-		<small> © Pénélope Ducharme, 2022 </small>
+		<div>  <?php get_sidebar( 'primary' ); ?></div>
+
+		<div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum maxime sapiente recusandae beatae soluta eum deleniti ullam, dolores alias excepturi commodi non consequatur perspiciatis laboriosam sit. Minima similique expedita sequi?</div>
+		<div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat recusandae, voluptatibus reiciendis doloribus quaerat inventore beatae, tempora ipsam vitae expedita, a ea sapiente dolorem quae minima et in quasi nulla?</div>
+		<div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat recusandae, voluptatibus reiciendis doloribus quaerat inventore beatae, tempora ipsam vitae expedita, a ea sapiente dolorem quae minima et in quasi nulla?</div>
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
@@ -20,3 +25,4 @@
 
 </body>
 </html>
+<!-- © Pénélope Ducharme, 2022 -->
