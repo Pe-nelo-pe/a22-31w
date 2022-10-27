55a5e9ed6f919f8f76c222b94f47335d3b0c3b58
Pénélope Ducharme <88218376+Pe-nelo-pe@users.noreply.github.com>
2022-10-27 15:38:47 -0400

    c14 - changement css du menu aside

--- sidebar-aside-1.php
+++ sidebar-aside-1.php
@@ -1,5 +1,6 @@
 
 
 <div id="sidebar-aside-1" class="sidebar">
+	<h2>Voir l'horaire</h2>
 	<?php dynamic_sidebar( 'aside-1' ); ?>
 </div>
--- sidebar-aside-2.php
+++ sidebar-aside-2.php
@@ -1,5 +1,6 @@
 
 
 <div id="sidebar-aside-2" class="sidebar">
+	<h2>Un peu de zoothérapie</h2>
 	<?php dynamic_sidebar( 'aside-2' ); ?>
 </div>
